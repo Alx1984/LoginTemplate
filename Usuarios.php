1e9ca552933899abcf2016ab53030740bcac5de4
Alx78 <alex.segura19@itca.edu.sv>
2021-04-16 01:47:16 -0600

    Login template

---
+++ Usuarios.php
@@ -0,0 +1,85 @@
+<?php
+    class Usuarios
+    {
+        private $usuarioId;
+        private $usuarioNombre;
+        private $usuarioPass;
+        private $usuarioNivel;
+
+        //constructor
+        function Usuarios(){
+
+        }
+
+        public function getUsuarioId()
+        {
+                return $this->usuarioId;
+        }
+        
+        public function getUsuarioNombre()
+        {
+                return $this->usuarioNombre;
+        }
+       
+        public function getUsuarioPass()
+        {
+                return $this->usuarioPass;
+        }
+        
+        public function getUsuarioNivel()
+        {
+                return $this->usuarioNivel;
+        }
+        
+        public function setUsuarioId($usuarioId)
+        {
+                $this->usuarioId = $usuarioId;
+
+                return $this;
+        }
+        
+        public function setUsuarioNombre($usuarioNombre)
+        {
+                $this->usuarioNombre = $usuarioNombre;
+
+                return $this;
+        }
+        
+        public function setUsuarioPass($usuarioPass)
+        {
+                $this->usuarioPass = $usuarioPass;
+
+                return $this;
+        }
+        
+        public function setUsuarioNivel($usuarioNivel)
+        {
+                $this->usuarioNivel = $usuarioNivel;
+
+                return $this;
+        }
+
+        //acciones de usuario
+        public function validar(){
+            $nivel = "";
+            $conexion = new mysqli('localhost', 'root','','sisv11');//conexion al sql
+            $sql= "SELECT nivel FROM usuarios WHERE nombreuser = '".$this->getUsuarioNombre()."' and pass = '".$this->getUsuarioPass()."'";
+            if ($resultado = $conexion->query($sql)) {
+                $usuarios = $resultado->fetch_assoc();//convierte a un arreglo asosiativo el resultado de la consulta
+                $nivel = $usuarios["nivel"];
+                //var_dump($nivel);// imprime el contenido de la variable
+                die;
+                return $nivel;
+            }else {
+                echo "No se ha podido conectar la DB";
+                return $nivel;
+                exit;
+            }
+        }
+     }
+
+    // $obj = new Usuarios();
+    // $obj-> setUsuarioNombre('Juan');
+    // $obj-> setUsuarioPass('123');
+    // $obj-> validar();    
+?>
---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+include 'Usuarios.php';
+$objUsuario = new Usuarios();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <h4>Login</h4><br>
+    <div class="form-control" style="width: 500px;">
+        <fieldset>
+            <legend>Formulario</legend> <br>
+            <form action="index.php" method="POST">
+                <label for="usuario">Usuario:</label>
+                <input type="text" name="txtUsuario" placeholder="Nombre de Usuario" class="form-control"> <br>
+                <label for="password">Contraseña:</label>
+                <input type="text" name="txtPassword" placeholder="Contraseña" class="form-control"> <br>
+                <input type="submit" name="btnLogin" value="Entrar" class="btn btn-success">
+
+            </form>
+
+
+        </fieldset>
+
+    </div>
+<?php
+
+if ($_POST) {
+    $usuario = $_REQUEST["txtUsuario"];
+    $contra = $_REQUEST["txtPassword"];
+    $objUsuario->setUsuarioNombre($usuario);
+    $objUsuario->setUsuarioPass($contra);
+    $nivel = $objUsuario->validar();
+
+    if ($nivel !="") {
+        $_SESSION["usuario"]["nivel"]=$nivel;
+        $_SESSION["usuario"]["usuario"]=$usuario;
+        header('location:index.php');
+    }else {
+        header('login.php');
+    }
+}
+
+
+
+?>
+</body>
+
+</html>
